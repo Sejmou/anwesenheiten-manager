aa0a8437096bef0190609a366305e1b669234053
Sejmou <samo.kolter@gmail.com>
2022-09-05 20:46:58 +0200

    try NextJS upgrade

--- pages/anwesenheiten/_middleware.ts
+++
@@ -1,2 +0,0 @@
-// this makes the whole route + subroutes unreachable to non-authenticated users: https://next-auth.js.org/tutorials/securing-pages-and-api-routes#nextjs-middleware
-export { default } from 'next-auth/middleware';
