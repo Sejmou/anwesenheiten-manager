a8046dda26e073348a36a07733d85459d0a6fc00
Sejmou <samo.kolter@gmail.com>
2022-09-05 12:17:33 +0200

    add possibility to choose event on attendance overview page

--- pages/anwesenheiten/index.tsx
+++ pages/anwesenheiten/index.tsx
@@ -2,28 +2,64 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import { NextPageWithLayout } from '../_app';
 import { getAuthenticatedPageLayout } from '../../components/layout/get-page-layouts';
+import { GetStaticProps } from 'next';
+import ResponsiveContainer from '../../components/layout/ResponsiveContainer';
+import { List, ListItemButton, ListItemText } from '@mui/material';
+import { useRouter } from 'next/router';
+import prisma from '../../lib/prisma';
+import { Event as EventDB } from '@prisma/client';
 
-// TODO: fetch choir practice schedule or something else from DB
-// export const getStaticProps: GetStaticProps = async () => {
-//   return {
-//     props: {},
-//     revalidate: 10, // enables Incremental Static Site Generation; will trigger update of served static page content if more than 10 seconds passed since last request by some client
-//   };
-// };
+type Event = Omit<EventDB, 'start' | 'end' | 'lastSyncAt'> & {
+  start: string;
+  end: string;
+};
 
-type Props = {};
+export const getStaticProps: GetStaticProps = async () => {
+  const events = (await prisma.event.findMany()).map(e => ({
+    ...e,
+    start: e.start.toLocaleString('de-AT'),
+    end: e.end.toLocaleString('de-AT'),
+    lastSyncAt: null, // we don't need that on the client
+  }));
 
-const Attendance: NextPageWithLayout = (props: Props) => {
+  return {
+    props: { events },
+    revalidate: 10, // enables Incremental Static Site Generation; will trigger update of served static page content if more than 10 seconds passed since last request by some client
+  };
+};
+
+type Props = { events: Event[] };
+
+const AttendanceEventOverview: NextPageWithLayout<Props> = ({
+  events,
+}: Props) => {
+  const router = useRouter();
+
   return (
     <>
-      <Typography variant="body1">
-        Auf dieser Seite wird es bald die Möglichkeit geben, Probenanwesenheiten
-        einzutragen.
+      <Typography>
+        Bitte wähle zuerst eine Probe/Veranstaltung für die du Anwesenheiten
+        eintragen möchtest.
       </Typography>
+      <ResponsiveContainer title="Termine">
+        <List>
+          {events.map((event, i) => (
+            <ListItemButton
+              key={i}
+              onClick={() => router.push(`anwesenheiten/${event.id}`)}
+            >
+              <ListItemText
+                primary={`${event.summary}, ${event.start}`}
+                secondary={`${event.location}`}
+              />
+            </ListItemButton>
+          ))}
+        </List>
+      </ResponsiveContainer>
     </>
   );
 };
 
-Attendance.getLayout = getAuthenticatedPageLayout;
+AttendanceEventOverview.getLayout = getAuthenticatedPageLayout;
 
-export default Attendance;
+export default AttendanceEventOverview;
